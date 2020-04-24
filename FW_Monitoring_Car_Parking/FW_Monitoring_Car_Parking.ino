cf8e1ab44e705edec5ae5fb4aeddfab8e90a3498
Dzn <nguyenhoangdat0809@gmail.com>
2020-04-24 10:33:13 +0700

    ok

--- FW_Monitoring_Car_Parking/FW_Monitoring_Car_Parking.ino
+++ FW_Monitoring_Car_Parking/FW_Monitoring_Car_Parking.ino
@@ -2,7 +2,7 @@
 #include <iNut.h>
 
 iNut sensor;
-#define DEBUG
+//#define DEBUG
 #define TimeCouter 2000
 #define LED_GREEN 2
 #define LED_RED 5
@@ -39,55 +39,60 @@ void setup() {
 
 void loop() {
   int state_sensor_infrared = digitalRead(4);
+  int state_sensor_infrared_2 = digitalRead(7);
   static unsigned long timer = 0;
   static unsigned long timerWatchDog = 0UL;
   static unsigned long RealTime = millis();
 
 #define State_Sensor_Unsignal            0
 #define State_Sensor_Receive_Signal      1
-#define State_Sensor_Done_Receive_Singal 2 
-//define all state of sensor
-static int State_Sensor = State_Sensor_Unsignal;
+#define State_Sensor_Done_Receive_Singal 2
+  //define all state of sensor
+  static int State_Sensor = State_Sensor_Unsignal;
 
-switch (State_Sensor){
-  case State_Sensor_Unsignal:{
-    Serial.println(F("Không Làm Gì"));
-    //do nothing;
-    State_Sensor = State_Sensor_Receive_Signal;
-    break;
-    }
-  case State_Sensor_Receive_Signal:{
-      if (state_sensor_infrared == 1) {
-    digitalWrite(LED_GREEN, HIGH);
-    digitalWrite(LED_RED, LOW);
-    sensor.setValue(0,state_sensor_infrared);
+  switch (State_Sensor) {
+    case State_Sensor_Unsignal: {
 #ifdef DEBUG
-      Serial.println("DATA - Không Có Xe Trong Bãi");
-      Serial.print("Data Sensor: ");
-      Serial.println(state_sensor_infrared);
-      Serial.println();
+        Serial.println(F("Không Làm Gì"));
 #endif
-   }
-      State_Sensor = State_Sensor_Done_Receive_Singal;
-      break;
-    }
-   case State_Sensor_Done_Receive_Singal:{
-    if (state_sensor_infrared == 0) {
-      digitalWrite(LED_GREEN, LOW);
-      digitalWrite(LED_RED, HIGH);
-      sensor.setValue(0,state_sensor_infrared);
+        //do nothing;
+        State_Sensor = State_Sensor_Receive_Signal;
+        break;
+      }
+    case State_Sensor_Receive_Signal: {
+        if (state_sensor_infrared == 1 || state_sensor_infrared_2 == 1) {
+          digitalWrite(LED_GREEN, HIGH);
+          digitalWrite(LED_RED, LOW);
 #ifdef DEBUG
-      Serial.println("DATA - Có Xe Trong Bãi");
-      Serial.print("Data Sensor: ");
-      Serial.println(state_sensor_infrared);
-      Serial.println();
+          Serial.println("DATA - Không Có Xe Trong Bãi");
+          Serial.print("Data Sensor: ");
+          Serial.println(state_sensor_infrared);
+          Serial.println();
 #endif
-    }
-    State_Sensor = State_Sensor_Unsignal;
-    break;
+        }
+        sensor.setValue(0, state_sensor_infrared);
+        sensor.setValue(1, state_sensor_infrared_2);
+        State_Sensor = State_Sensor_Done_Receive_Singal;
+        break;
+      }
+    case State_Sensor_Done_Receive_Singal: {
+        if (state_sensor_infrared == 0 || state_sensor_infrared_2 == 0) {
+          digitalWrite(LED_GREEN, LOW);
+          digitalWrite(LED_RED, HIGH);
+#ifdef DEBUG
+          Serial.println("DATA - Có Xe Trong Bãi");
+          Serial.print("Data Sensor: ");
+          Serial.println(state_sensor_infrared);
+          Serial.println();
+#endif
+        }
+        sensor.setValue(0, state_sensor_infrared);
+        sensor.setValue(1, state_sensor_infrared_2);
+        State_Sensor = State_Sensor_Unsignal;
+        break;
+      }
   }
-}
-  
+
 
 
   if (RealTime > TimeCouter) {
@@ -101,27 +106,39 @@ switch (State_Sensor){
   if (millis() - timerWatchDog > 2000UL) {
     wdt_reset();
     timerWatchDog = millis();
-    
+
 #ifdef DEBUG
     Serial.println('.');
 #endif
   }
-  
+
   sensor.loop();
   getMemoryFree();
   wdt_reset();
-  if (millis() - timer > 2000UL){
+  if (millis() - timer > 1000UL) {
     timer = millis();
-    Serial.print ("Dữ Liệu Số Của Cảm Biến:  ");
-    Serial.println(state_sensor_infrared);
+    //    Serial.print ("Dữ Liệu Số Của Cảm Biến:  ");
+    //    Serial.println(state_sensor_infrared);
+    if (state_sensor_infrared == 1 && state_sensor_infrared_2 == 1 ) {
+      Serial.println("Đéo có cái đen xe nào hết địt mẹ");
+      Serial.println();
+    }
+    else if (state_sensor_infrared == 0) {
+      Serial.println("Có cái xe đen đậu ngu vãi lồn");
+      Serial.println();
+    }
+    else {
+      Serial.println("Có cái xe vàng đậu ngu vãi lồn");
+      Serial.println();
+    }
   }
   //= 0 => có vật cản, bằng 1 => 0 có vật cản
 
 }
 
-int getMemoryFree(){
-extern int _heap_start;
-extern int *_brkval;
-//get free memory from function
-return (int) SP - (_brkval == 0 ? (int) &_heap_start : (int) _brkval);
+int getMemoryFree() {
+  extern int _heap_start;
+  extern int *_brkval;
+  //get free memory from function
+  return (int) SP - (_brkval == 0 ? (int) &_heap_start : (int) _brkval);
 }
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+2 cảm biến hồng ngoại để theo dõi 2 xe
+gửi data lên node red ide thông qua local server raspbery pi
+chi tiết: updating
